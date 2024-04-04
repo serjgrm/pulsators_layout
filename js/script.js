8e48ce1a0ea6ee214c20746737a8d9a1c3e21053
Serhii Gramma <grammas56@gmail.com>
2024-04-04 12:23:19 +0300

    validation fixed

--- js/script.js
+++ js/script.js
@@ -32,12 +32,12 @@ $(document).ready(function(){
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         })
     });
-};
+  };
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 
-// Modal
+  // Modal
 
   $('[data-modal=consultation]').on('click', function(){
     $('.overlay, #consultation').fadeIn('fast');
@@ -52,61 +52,61 @@ toggleSlide('.catalog-item__back');
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('slow');
     })
-});
+  });
 
-function validateForms(form){
-  $(form).validate({
-      rules: {
-          name: {
-              required: true,
-              minlength: 2
-          },
-          phone: "required",
-          email: {
-              required: true,
-              email: true
-          }
-      },
-      messages: {
-          name: {
-              required: "Пожалуйста, введите свое имя",
-              minlength: jQuery.validator.format("Введите {0} символа!")
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
             },
-          phone: "Пожалуйста, введите свой номер телефона",
-          email: {
-            required: "Пожалуйста, введите свою почту",
-            email: "Неправильно введен адрес почты"
-          }
-      }
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter your name",
+                minlength: jQuery.validator.format("Enter at least {0} characters!")
+              },
+            phone: "Please enter your phone number",
+            email: {
+              required: "Please enter your email",
+              email: "Invalid email address"
+            }
+        }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    if (!$(this).valid()){
+      return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
   });
-};
 
-validateForms('#consultation-form');
-validateForms('#consultation form');
-validateForms('#order form');
-
-$('input[name=phone]').mask("+7 (999) 999-99-99");
-
-$('form').submit(function(e) {
-  e.preventDefault();
-  if (!$(this).valid()){
-    return;
-  }
-
-  $.ajax({
-      type: "POST",
-      url: "mailer/smart.php",
-      data: $(this).serialize()
-  }).done(function() {
-      $(this).find("input").val("");
-      $('#consultation, #order').fadeOut();
-      $('.overlay, #thanks').fadeIn('slow');
-      $('form').trigger('reset');
-  });
-  return false;
-});
-
-// smooth scroll and pageup
+  // smooth scroll and pageup
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 1600) {
@@ -120,8 +120,7 @@ $('form').submit(function(e) {
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
-});
+  });
 
-new WOW().init();
+  new WOW().init();
 });
-
